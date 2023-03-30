7d0e76e34fb749c6fcedc3fe6dc84e739a933a90
max reid <maxwilliamreid@gmail.com>
2023-03-30 11:09:49 +1100

    removed active storage from project

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,11 @@ require "rails"
 
 %w(
   active_record/railtie
-
   action_controller/railtie
   action_view/railtie
   action_mailer/railtie
   active_job/railtie
   action_cable/engine
-
-  action_text/engine
   rails/test_unit/railtie
 ).each do |railtie|
   begin
