34a1892e762414c67d4cdc3566f9aafc3ce876c4
max reid <maxwilliamreid@gmail.com>
2023-03-30 11:07:04 +1100

    working on scrapelist controller easy functions, added routes, removed action mailbox from config application.rb

--- app/controllers/scrapelistprompts_controller.rb
+++ app/controllers/scrapelistprompts_controller.rb
@@ -1,2 +1,31 @@
 class ScrapelistpromptsController < ApplicationController
+  def new_easy
+    @scrapelist = Scrapelistprompt.new
+  end
+
+  def create_easy
+    @scrapelist = Scrapelistprompt.new(scrapelist_params_easy)
+    @scrapelist.subgenre = ''
+    @scrapelist.release_order = "new"
+    @scrapelist.page_number = 0
+    @scrapelist.location = 0
+    @scrapelist.time_frame = 0
+    @scrapelist.spotify_account = 'TODO'
+    @scrapelist.bandcamp_query = "https://bandcamp.com/?g=#{@scrapelist.genre}&s=#{@scrapelist.release_order}&p=#{@scrapelist.page_number}&gn=#{@scrapelist.location}&f=all&w=#{@scrapelist.time_frame}"
+    @scrapelist.save!
+  end
+
+  def show
+    @scrapelist = Scrapelistprompt.find(params[:id])
+  end
+
+  private
+
+  def scrapelist_params_easy
+    params.require(:scrapelistprompt).permit(:spotify_account, :genre)
+  end
+
+  def scrapelist_params_picky
+    params.require(:scrapelistprompt).permit(:spotify_account, :genre, :subgenre, :release_order, :time_frame, :location)
+  end
 end
