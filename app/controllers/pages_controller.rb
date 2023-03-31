f245b8c2f6f6000390afa7401e6b323ccdd80bd5
max reid <maxwilliamreid@gmail.com>
2023-03-31 18:29:04 +1100

    created login action logic so that the user is sent to the spotify verify page, then afterwards returned to the scrapelists index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   def home; end
 
-  def login; end
+  def login
+    authorise = 'https://accounts.spotify.com/authorize'
+    clientID = '3f15114ecf52461a95ef44a6b24976d7'
+    url = authorise
+    url += "?client_id=#{clientID}"
+    url += '&response_type=code'
+    url += '&redirect_uri=http://127.0.0.1:3000/scrapelists'
+    url += '&show_dialog=true'
+    url += '&scope=playlist-read-private'
+    redirect_to url, allow_other_host: true
+  end
 end
