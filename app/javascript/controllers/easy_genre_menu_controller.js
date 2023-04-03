7a6aec277b85e2b24be413f26fd4b3f17a63a63a
max reid <maxwilliamreid@gmail.com>
2023-04-04 08:50:56 +1000

    refactored the easy_genre_menu js controller with comments outlining what is going on

--- app/javascript/controllers/easy_genre_menu_controller.js
+++ app/javascript/controllers/easy_genre_menu_controller.js
@@ -5,24 +5,24 @@ let GROUPCOUNT = 0;
 // Connects to data-controller="easy-genre-menu"
 export default class extends Controller {
   connect() {
-    console.log('easy-genre-menu controller connected')
-    // const surpriseButton = document.getElementById('scrapelistprompt_genre_surprise');
-    // selectRandomRadioButton(surpriseButton);
-    // console.log(surpriseButton);
+    // reset the iterator to 0 whenever the controller is connected
+    GROUPCOUNT = 0;
   }
 
   scrollUp() {
-    console.log('clicked up');
+    // define the buttons
     const upButton = document.querySelector('.up-easy-menu-action');
     const downButton = document.querySelector('.down-easy-menu-action');
     const genreGroup = document.querySelectorAll('.easy-genre-menu-group');
+    // hide the current group
     genreGroup[GROUPCOUNT].setAttribute('id', 'inactive-list-group');
+    // show the previous group if there is one
     if (genreGroup[GROUPCOUNT - 1] != null) {
       genreGroup[GROUPCOUNT - 1].removeAttribute('id', 'inactive-list-group');
     }
+    // redefine the iterator after the change is shown to reflect where on the menu the user currently is
     GROUPCOUNT -= 1;
-    console.log(GROUPCOUNT);
-    // upButton.removeAttribute('id', 'inactive-button');
+    // hide the up button if user is at the top of the menu
     if (GROUPCOUNT == 0) {
       upButton.setAttribute('id', 'inactive-button');
     } else {
@@ -32,18 +32,19 @@ export default class extends Controller {
   }
 
   scrollDown() {
-    console.log('clicked down');
+    // define the buttons
     const upButton = document.querySelector('.up-easy-menu-action');
     const downButton = document.querySelector('.down-easy-menu-action');
     const genreGroup = document.querySelectorAll('.easy-genre-menu-group');
+    // hide the current group
     genreGroup[GROUPCOUNT].setAttribute('id', 'inactive-list-group');
+    // show the next group if there is one
     if (genreGroup[GROUPCOUNT + 1] != null) {
       genreGroup[GROUPCOUNT + 1].removeAttribute('id', 'inactive-list-group');
-    } else if (genreGroup[GROUPCOUNT - 1] != null) {
-      genreGroup[GROUPCOUNT - 1].setAttribute('id', 'inactive-list-group');
     }
+    // redefine the iterator after the change is shown to reflect where on the menu the user currently is
     GROUPCOUNT += 1;
-    console.log(GROUPCOUNT);
+    // hide the down button if user is at the bottom of the menu
     upButton.removeAttribute('id', 'inactive-button');
     if (GROUPCOUNT == 2) {
       downButton.setAttribute('id', 'inactive-button');
@@ -54,15 +55,17 @@ export default class extends Controller {
   }
 
   selectSurprise() {
+    // define the radio buttons and the surprise button
     const radioButtons = document.getElementsByName('scrapelistprompt[genre]');
     const surpriseButton = document.getElementById('scrapelistprompt_genre_surprise');
+    // create a random number between 0 and the number of radio buttons (not including the surprise button)
     const rand = Math.floor(Math.random() * (radioButtons.length - 1));
-    // console.log(radioButtons[5]);
-    // console.log(rand);
     if (rand == 0) {
+      // if the random number is 0 (which would be the ALL button), set the surprise button value to acoustic
       surpriseButton.value = "acoustic"
       console.log(surpriseButton);
     } else {
+      // otherwise, set the surprise button value to the value of a random radio button
       const randomValue = radioButtons[rand].value;
       surpriseButton.value = randomValue;
       console.log(surpriseButton);
