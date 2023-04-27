d047bb8bf3cf902ea6edc63959e19e555f013151
max reid <maxwilliamreid@gmail.com>
2023-04-27 15:44:50 +1000

    repaired broken js controller functions from changing some of the css rules

--- app/javascript/controllers/picky_genre_menu_controller.js
+++ app/javascript/controllers/picky_genre_menu_controller.js
@@ -22,10 +22,10 @@ export default class extends Controller {
     const downButton = document.querySelector('.down-picky-menu-action');
     const genreGroup = document.querySelectorAll('.picky-genre-menu-group');
     // hide the current group
-    genreGroup[GROUPCOUNT].setAttribute('id', 'inactive-list-group');
+    genreGroup[GROUPCOUNT].setAttribute('id', 'inactive-list-group-picky');
     // show the previous group if there is one
     if (genreGroup[GROUPCOUNT - 1] != null) {
-      genreGroup[GROUPCOUNT - 1].removeAttribute('id', 'inactive-list-group');
+      genreGroup[GROUPCOUNT - 1].removeAttribute('id', 'inactive-list-group-picky');
     }
     // redefine the iterator after the change is shown to reflect where on the menu the user currently is
     GROUPCOUNT -= 1;
@@ -45,10 +45,10 @@ export default class extends Controller {
     const downButton = document.querySelector('.down-picky-menu-action');
     const genreGroup = document.querySelectorAll('.picky-genre-menu-group');
     // hide the current group
-    genreGroup[GROUPCOUNT].setAttribute('id', 'inactive-list-group');
+    genreGroup[GROUPCOUNT].setAttribute('id', 'inactive-list-group-picky');
     // show the next group if there is one
     if (genreGroup[GROUPCOUNT + 1] != null) {
-      genreGroup[GROUPCOUNT + 1].removeAttribute('id', 'inactive-list-group');
+      genreGroup[GROUPCOUNT + 1].removeAttribute('id', 'inactive-list-group-picky');
     }
     // redefine the iterator after the change is shown to reflect where on the menu the user currently is
     GROUPCOUNT += 1;
@@ -87,10 +87,10 @@ export default class extends Controller {
     // define the windows
     const windows = document.querySelectorAll('.form-window');
     // hide the current window
-    windows[WINDOWCOUNT].setAttribute('id', 'display-none');
+    windows[WINDOWCOUNT].setAttribute('id', 'display-none-picky');
     // show the next window if there is one
     if (windows[WINDOWCOUNT + 1] != null) {
-      windows[WINDOWCOUNT + 1].removeAttribute('id', 'display-none');
+      windows[WINDOWCOUNT + 1].removeAttribute('id', 'display-none-picky');
     }
     // redefine the iterator after the change is shown to reflect where on the menu the user currently is
     WINDOWCOUNT += 1;
@@ -106,10 +106,10 @@ export default class extends Controller {
     // define the windows
     const windows = document.querySelectorAll('.form-window');
     // hide the current window
-    windows[WINDOWCOUNT].setAttribute('id', 'display-none');
+    windows[WINDOWCOUNT].setAttribute('id', 'display-none-picky');
     // show the previous window if there is one
     if (windows[WINDOWCOUNT - 1] != null) {
-      windows[WINDOWCOUNT - 1].removeAttribute('id', 'display-none');
+      windows[WINDOWCOUNT - 1].removeAttribute('id', 'display-none-picky');
     }
     // redefine the iterator after the change is shown to reflect where on the menu the user currently is
     WINDOWCOUNT -= 1;
@@ -124,20 +124,20 @@ export default class extends Controller {
     const when_container = document.querySelector('.when-container');
     // hide all the subgenre buttons everytime this function is called
     subgenre_buttons.forEach((button) => {
-      button.setAttribute('id', 'display-none');
+      button.setAttribute('id', 'display-none-picky');
     });
     // select which subgenre buttons to show based on which genre button is checked
     genre_buttons.forEach((button) => {
       if (button.checked && button.value == 'all') {
-        subgenre_container.setAttribute('id', 'display-none');
-        release_type_container.removeAttribute('id', 'visibility-hidden');
-        when_container.removeAttribute('id', 'visibility-hidden');
-        when_buttons_container.removeAttribute('id', 'visibility-hidden');
+        subgenre_container.setAttribute('id', 'display-none-picky');
+        release_type_container.removeAttribute('id', 'visibility-hidden-picky');
+        when_container.removeAttribute('id', 'visibility-hidden-picky');
+        when_buttons_container.removeAttribute('id', 'visibility-hidden-picky');
         console.log('all genres selected');
       } else if (button.checked) {
-        subgenre_container.removeAttribute('id', 'display-none');
+        subgenre_container.removeAttribute('id', 'display-none-picky');
         const subgenre_toshow = document.querySelector(`.${button.value}-subgenres`);
-        subgenre_toshow.removeAttribute('id', 'display-none');
+        subgenre_toshow.removeAttribute('id', 'display-none-picky');
         console.log(`${button.value} selected`);
       }
     });
@@ -150,20 +150,20 @@ export default class extends Controller {
     const when_container = document.querySelector('.when-container');
     const when_buttons_container = document.querySelector('.when-buttons-container');
     // reset the release_type and when containers to hidden
-    release_type_container.setAttribute('id', 'visibility-hidden');
-    when_container.setAttribute('id', 'visibility-hidden');
+    release_type_container.setAttribute('id', 'visibility-hidden-picky');
+    when_container.setAttribute('id', 'visibility-hidden-picky');
     // set a click event for if the user clicks any of the subgenre_buttons
     subgenre_buttons.forEach((button) => {
       button.addEventListener('click', (event) => {
         // unhide the release_type and when containers when a subgenre button is clicked
-        release_type_container.removeAttribute('id', 'visibility-hidden');
-        when_container.removeAttribute('id', 'visibility-hidden');
+        release_type_container.removeAttribute('id', 'visibility-hidden-picky');
+        when_container.removeAttribute('id', 'visibility-hidden-picky');
         // if the user clicks the ALL button, show the when_buttons_container
         if (button.value.endsWith("~all")) {
-          when_buttons_container.removeAttribute('id', 'visibility-hidden');
+          when_buttons_container.removeAttribute('id', 'visibility-hidden-picky');
         } else {
           // if the user clicks any other button, hide the when_buttons_container
-          when_buttons_container.setAttribute('id', 'visibility-hidden');
+          when_buttons_container.setAttribute('id', 'visibility-hidden-picky');
           // and set the this_week button to checked
           const this_week_btn = document.getElementById('scrapelistprompt_time_frame_0')
           this_week_btn.checked = true;
